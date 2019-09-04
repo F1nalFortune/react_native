5b14cb8413aab057e17aad74563018b00daaae20
Brian <juniortoad1738@gmail.com>
2019-09-04 14:35:05 -0400

    reattached head

---
+++ src/components/NewDirections.js
@@ -0,0 +1,31 @@
+//This is an example code to get Geolocation//
+import React, { Component } from 'react';
+//import react in our code.
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image ,
+  PermissionsAndroid,
+  Platform,
+  AppRegistry,
+  Dimensions,
+  StatusBar,
+  Linking,
+  TouchableOpacity,
+  ScrollView,
+  Button
+} from 'react-native';
+//import all the components we are going to use.
+
+
+
+export default class Directions extends Component {
+
+render(){
+  return(
+    
+  )
+}
+
+}
